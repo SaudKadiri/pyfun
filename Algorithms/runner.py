3b3d7c9b4496fc09b49581a7c6c9542f42ba32e7
Saud Kadiri <34000860+SaudKadiri@users.noreply.github.com>
2022-07-24 23:01:24 +0530

    Update runner.py

--- Algorithms/runner.py
+++ Algorithms/runner.py
@@ -1,22 +1,20 @@
-from comparison_sorts import *
+import comparison_sorts
 import random
+from timeit import default_timer as timer
+from datetime import timedelta
 
 if __name__ == '__main__':
-    # lst = [1, 2, 5, 6, 0, 3, 6, 4, 10, -9]
-    lst = [*range(-12, 12)]
+    sorts = [(x.replace('_', ' ').capitalize(), y) for x, y in comparison_sorts.__dict__.items() if 'sort' in x]
+    lst = [*range(-24000, 24000)]
     random.shuffle(lst)
-    print('Intitially:', lst)
-    print('-'*120, '+', sep='')
-    print('Algorithm\t|\t\t\t\t\t\t\tOutput\t\t\t\t\t\t|')
-    print('-'*120, '+', sep='')
-    print('Selection sort\t|\t', selection(lst.copy()), '\t|')
-    print('Bubble sort\t|\t', bubble(lst.copy()), '\t|')
-    print('Insertion sort\t|\t', insertion(lst.copy()), '\t|')
-    print('Shell sort\t|\t', shellsort(lst.copy()), '\t|')
-    print('Exchange sort\t|\t', exchange_sort(lst.copy()), '\t|')
-    print('Gnome sort\t|\t', gnome_sort(lst.copy()), '\t|')
-    print('Even-odd sort\t|\t', even_odd(lst.copy()), '\t|')
-    print('Heap sort\t|\t', heapsort(lst.copy()), '\t|')
-    print('Merge sort\t|\t', merge_sort(lst.copy()), '\t|')
-    print('Quick sort\t|\t', quicksort(lst.copy()), '\t|')
-    print('-'*120, '+', sep='')
+    print('Intitially:', lst if len(lst) <= 24 else '...')
+    print('-'*139, '+', sep='')
+    print('Algorithm\t|\t\t\t\t\t\t\tOutput\t\t\t\t\t\t|\tTime\t\t|')
+    print('-'*139, '+', sep='')
+    for name, algo in sorts:
+        start = timer()
+        ans = algo(lst.copy())
+        end = timer()
+        print(f'{name}\t|\t', ans if len(ans) <= 24 else '\t\t\t\t\t\t...\t\t\t\t\t', '\t|', end=' ')
+        print(timedelta(seconds=end-start), end='\t|\n')  
+        print('-'*139, '+', sep='')
