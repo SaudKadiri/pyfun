f9a9b21fe07cbc9d452229aa04812211dc58f963
Saud Kadiri <34000860+SaudKadiri@users.noreply.github.com>
2022-07-25 19:56:27 +0530

    Update binary_tree.py

--- DS/Non_Linear/binary_tree.py
+++ DS/Non_Linear/binary_tree.py
@@ -1,11 +1,18 @@
 class Node:
   def __init__(self, val=0, left=None, right=None):
-        """Initializes the vals and nxt"""
-        self.val = val
-        self.left = left
-        self.right = right
+    """Initializes the vals and nxt"""
+    self.val = val
+    self.left = left
+    self.right = right
     
-    def __repr__(self):
-        """For user-friendly output"""
-        return 'Node({})'.format(self.val)
+  def __repr__(self):
+    """For user-friendly output"""
+    return 'Node({})'.format(self.val)
     
+class BinaryTree:
+  def __init__(self, root=Node()):
+    self.root = root
+  def insert(self, node):
+    pass
+    
+      
