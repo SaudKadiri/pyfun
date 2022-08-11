12a425bf8c160fd15ddb15247874101713906fed
Saud <34000860+SaudKadiri@users.noreply.github.com>
2022-08-11 23:29:05 +0530

    Support for dsorts & move the file to sorts folder

--- Algorithms/sorts/runner.py
+++ Algorithms/sorts/runner.py
@@ -1,13 +1,15 @@
-import comparison_sorts
+import comparison_sorts as csorts
 import random
 from timeit import default_timer as timer
 from datetime import timedelta
+import distribution_sorts as dsorts
 
 if __name__ == '__main__':
+    is_sorted = lambda lst: all([prev <= curr for prev, curr in zip(lst, lst[1:])])
     lst = [*range(-12, 12)] + [*range(-120, 120)]
     random.shuffle(lst)
     sorts = {}
-    for name, algo in comparison_sorts.__dict__.items():
+    for name, algo in (csorts.__dict__ | dsorts.__dict__).items():
         if 'sort' in name:
             name = name.replace('_', ' ').capitalize()
             print('Performing:', name, '...')
@@ -24,6 +26,7 @@ if __name__ == '__main__':
     print('-'*139, '+', sep='')
 
     for name, info in sorts:
-        print(f'{name}\t|\t', info["output"] if len(info["output"]) <= 24 else '\t\t\t\t\t\t...\t\t\t\t\t', '\t|', end=' ')
+        print(f'{name}\t|\t', info["output"] if len(info["output"]) <= 24 else f'\t\t\t\t\t\t{is_sorted(info["output"])}\t\t\t\t\t', '\t|', end=' ')
         print(info["time"], end='\t|\n')  
         print('-'*139, '+', sep='')
+    
