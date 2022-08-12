fa319e83dc9f9cb8a505ff370531ee2e105a3dcf
Saud <34000860+SaudKadiri@users.noreply.github.com>
2022-08-12 21:42:11 +0530

    Prepend along with the docstrings

--- DS/Linear/dll.py
+++ DS/Linear/dll.py
@@ -1,3 +1,6 @@
+# Author: Saud
+# Reference: https://en.wikipedia.org/wiki/Doubly_linked_list
+
 class Node:
     def __init__(self, val, nxt=None, pre=None):
         """Initializes the vals and nxt"""
@@ -11,27 +14,47 @@ class Node:
 
 class DoublyLinkedList:
     def __init__(self, head=None, tail=None) -> None:
+        """If some head or tail is passed initializes it with it; else assigns None"""
         self.head = head or tail    # points to first node of list
         self.tail = tail or head    # points to last node of list
 
     def insert_after(self, node, new_node):
+        """Insert the new_node after node"""
         new_node.pre = node 
         new_node.nxt = node.nxt   
         if node is self.tail:
             self.tail = new_node
         else:
-            node.nxt.prev = new_node
+            node.nxt.pre = new_node
         node.nxt = new_node
-    
+
     def append(self, item):
+        """Insertion at the rear (right) end of the list"""
         node = item if type(item) is Node else Node(item)
         if self.tail is None:
             self.head = self.tail = node
         else:
             self.insert_after(self.tail, node)
 
+    def insert_before(self, node, new_node):
+        """Inser a new_node before node"""
+        new_node.nxt = node
+        new_node.pre = node.pre
+        if node is self.head:
+            self.head = new_node
+        else:
+            node.pre.nxt = new_node
+        node.pre = new_node
+
+    def prepend(self, item):
+        """Insert at the front (left) end of the list"""
+        node = item if type(item) is Node else Node(item)
+        if self.head is None:
+            self.head = self.tail = node
+        else:
+            self.insert_before(self.head, node)
+
     def __str__(self):
-        # Only for the time being the following prints two values; as it's in testing period once everything is done; a logical str will be returned
         """User-friendly output"""
         trav, nodes = self.head, []
         while trav:
@@ -48,10 +71,11 @@ class DoublyLinkedList:
 
 if __name__ == '__main__':
     dll = DoublyLinkedList()
-    dll.append(10)
+    dll.prepend(10)
     dll.insert_after(dll.head, Node(20))
-    dll.append(30)
-    dll.append(40)
-    dll.append(90)
-    dll.append(100)
+    dll.prepend(30)
+    dll.prepend(40)
+    dll.prepend(90)
+    dll.prepend(100)
+    dll.prepend(1110)
     print(dll)
